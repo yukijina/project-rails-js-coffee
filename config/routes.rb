112ebfd872f453784fd6a416b50955e80e34cd62
yukijina <yukijina@gmail.com>
2019-07-21 09:06:26 -0700

    add session(sign in) and sing up (user#new) routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
+
+  get 'sessions/signin'
+  get  '/signup', to: 'users#new'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
