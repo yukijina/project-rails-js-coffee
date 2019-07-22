998d8ada2ae625fd14fc83da39a4541678bb2854
yukijina <yukijina@gmail.com>
2019-07-21 19:55:49 -0700

    update create and bean_params (roaster is now nested/roaster_attributes)

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,2 +1,20 @@
 class BeansController < ApplicationController
+  def index
+    @beans = Bean.all
+  end
+
+  def new
+    @bean = Bean.new
+  end
+
+  def create
+    @bean = Bean.new(bean_params)
+    @roaster = @bean.build_roaster(bean_params[:roaster_attributes])
+  end
+
+  private
+  def bean_params
+    params.require(:bean).permit(:brand, :variety, :taste_note, :description, :organic, :fairtrade, :roaster_id, :roaster_attributes => [:roaster_name, :city, :state, :url])
+  end
+
 end
