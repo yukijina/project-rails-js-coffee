11087e4474883a61bdb98550be7b4204ba61c3b4
yukijina <yukijina@gmail.com>
2019-07-21 19:53:03 -0700

    drop table, column change

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190721222419) do
+ActiveRecord::Schema.define(version: 20190722014915) do
 
   create_table "beans", force: :cascade do |t|
     t.string   "brand"
     t.string   "variety"
     t.string   "taste_note"
-    t.string   "description"
+    t.text     "description"
     t.boolean  "organic"
     t.boolean  "fairtrade"
     t.integer  "roaster_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "location_1"
-    t.string   "location_2"
-    t.string   "location_3"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "bean_id"
+    t.string   "origin_1"
+    t.string   "origin_2"
+    t.string   "origin_3"
   end
 
   create_table "roasters", force: :cascade do |t|
