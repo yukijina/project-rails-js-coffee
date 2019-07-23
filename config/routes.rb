711fc0840a4bb38bf80f59e06d346dc3e794bf73
yukijina <yukijina@gmail.com>
2019-07-23 07:55:40 -0700

    change user routes. new user -> /signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   post '/signin', to: 'sessions#create'
   get  '/signup', to: 'users#new'
 
-  resources :users
+  resources :users, only: [:show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
