09cd0434a9adfbf96cc6b5f99802d8441cbf90b3
yukijina <yukijina@gmail.com>
2019-07-27 15:17:21 -0700

    add has_many users relationship

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,5 +1,6 @@
 class Bean < ApplicationRecord
   belongs_to :roaster
   has_many :favorite_and_comments
+  has_many :users, through: :favorite_and_comments
   accepts_nested_attributes_for :roaster
 end
