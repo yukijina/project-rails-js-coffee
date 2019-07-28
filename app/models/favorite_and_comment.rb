d68d82b0d63ac9f46db798122fc63e6b9a25d1c1
yukijina <yukijina@gmail.com>
2019-07-28 13:02:02 -0700

    change name of method

--- app/models/favorite_and_comment.rb
+++ app/models/favorite_and_comment.rb
@@ -2,7 +2,7 @@ class FavoriteAndComment < ApplicationRecord
   belongs_to :user
   belongs_to :bean
 
-  def self.find_favorite(user, bean)
+  def self.find_matching_data(user, bean)
     where(["user_id = ? AND bean_id = ?", user, bean])
   end
 end
