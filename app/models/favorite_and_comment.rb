a47f742e80172923df22111594a5b9ae585c6f13
yukijina <yukijina@gmail.com>
2019-07-27 21:52:31 -0700

    find user's favorite

--- app/models/favorite_and_comment.rb
+++ app/models/favorite_and_comment.rb
@@ -1,4 +1,8 @@
 class FavoriteAndComment < ApplicationRecord
   belongs_to :user
   belongs_to :bean
+
+  def self.find_favorite(user, bean)
+    where(["user_id = ? AND bean_id = ?", user, bean])
+  end
 end
