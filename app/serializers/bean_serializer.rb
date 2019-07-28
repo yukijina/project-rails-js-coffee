6a8d6a1299a395e0e252081801efce6cfe263b60
yukijina <yukijina@gmail.com>
2019-07-27 21:48:36 -0700

    add relationship

--- app/serializers/bean_serializer.rb
+++ app/serializers/bean_serializer.rb
@@ -1,4 +1,6 @@
 class BeanSerializer < ActiveModel::Serializer
   attributes :id, :brand, :variety, :taste_note, :description, :organic, :fairtrade, :origin_1, :origin_2, :origin_3
   belongs_to :roaster
+  has_many :favorite_and_comments
+  has_many :users, through: :favorite_and_comments
 end
