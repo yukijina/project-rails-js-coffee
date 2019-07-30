e21960977f3f624ac2272bffde2e394e4ece1e39
yukijina <yukijina@gmail.com>
2019-07-30 06:45:38 -0700

    create method for user. User is now nested under favorite_and_comments

--- app/serializers/favorite_and_comment_serializer.rb
+++ app/serializers/favorite_and_comment_serializer.rb
@@ -1,12 +1,10 @@
 class FavoriteAndCommentSerializer < ActiveModel::Serializer
-  attributes :id, :favorite, :comments#, :user
+  attributes :id, :favorite, :comments, :user
   belongs_to :user
   belongs_to :bean
 
-  # def user
-  #   {
-  #     user_id: self.object.user_id,
-  #     username: self.scope.username
-  #   }
-  # end
+  def user
+    { user_id: self.object.user_id,
+      username: self.scope.username }
+  end
 end
