884fe8a821351001c815394cc62ab4c6d2b0a382
yukijina <yukijina@gmail.com>
2019-07-30 21:10:42 -0700

    update user serializer. Nested under favorite_and_comments

--- app/serializers/favorite_and_comment_serializer.rb
+++ app/serializers/favorite_and_comment_serializer.rb
@@ -1,10 +1,22 @@
 class FavoriteAndCommentSerializer < ActiveModel::Serializer
   attributes :id, :favorite, :comments, :user
-  belongs_to :user
+  #belongs_to :user
   belongs_to :bean
 
-  def user
-    { user_id: self.object.user_id,
-      username: self.scope.username }
-  end
+   def user
+     id = self.object.user_id
+     { user_id: id,
+       username: User.find_by(id: id).username  }
+   end
 end
+
+
+##self.object
+#<FavoriteAndComment:0x00007fc732958e80
+ #id: 1,
+ #favorite: true,
+ #comments: "I love this beans. Fresh light roast, amazing flavor.",
+ #user_id: 1,
+ #bean_id: 1,
+ #created_at: Mon, 29 Jul 2019 01:31:14 UTC +00:00,
+ #updated_at: Mon, 29 Jul 2019 01:31:14 UTC +00:00>
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
   has_many :favorite_and_comments
-  has_many :beans, through: :favorite_and_comments
+  has_many :beans
 end
