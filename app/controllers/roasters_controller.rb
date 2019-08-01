edadbf1d961137305869859712f41c86ce0fbebf
yukijina <yukijina@gmail.com>
2019-08-01 06:41:43 -0700

    add index

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -1,2 +1,12 @@
 class RoastersController < ApplicationController
+
+  def index
+    @roasters = Roaster.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @roasters }
+    end
+  end
+
+
 end
