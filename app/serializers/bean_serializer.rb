7f7ab17854b72d93c1176eceeebce8abd88d5908
yukijina <yukijina@gmail.com>
2019-08-02 09:21:31 -0700

    remove fairtrade attribute

--- app/serializers/bean_serializer.rb
+++ app/serializers/bean_serializer.rb
@@ -1,5 +1,5 @@
 class BeanSerializer < ActiveModel::Serializer
-  attributes :id, :brand, :variety, :taste_note, :description, :organic, :fairtrade, :origin_1, :origin_2, :origin_3
+  attributes :id, :brand, :variety, :taste_note, :description, :organic, :origin_1, :origin_2, :origin_3
   belongs_to :roaster
   has_many :favorite_and_comments
   #has_many :users
